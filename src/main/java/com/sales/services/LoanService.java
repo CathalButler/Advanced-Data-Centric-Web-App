994802bb40dd1ccdc2a6105e1aad9a96f286ab7c
Cathal <cathalbutler@outlook.com>
2019-04-26 19:46:10 +0100

    Loan Duration stamp added to books on loan.

--- src/main/java/com/sales/services/LoanService.java
+++ src/main/java/com/sales/services/LoanService.java
@@ -5,15 +5,33 @@ import com.sales.repositories.LoanRepoInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
 @Service
 public class LoanService {
 
     @Autowired
     LoanRepoInterface li;
 
+
     // Method to save loans
     public void save(Loan l) {
+        // Loan Peroid
+        int loanDuration = l.getCust().getLoanPeriod();
+        // Get localDate + loan Duration
+        LocalDate date = LocalDate.now().plusDays(loanDuration);
+        // Format localDate
+        String formattedDate = date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        // Set the Due date to the loan object
+        l.setDueDate(formattedDate);
+        // Save the object to the database
         li.save(l);
+
+
     }// End save
 
     // Method to get all customers
